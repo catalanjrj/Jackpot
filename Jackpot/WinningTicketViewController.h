9177633af8a83b16b5a96f691cdfdcc398385cbb
Jorge Catalan Jr <catalanjrj@gmail.com>
2016-04-21 09:36:13 -0400

    replaced viewController with WinningViewConroller

---
+++ Jackpot/WinningTicketViewController.h
@@ -0,0 +1,13 @@
+//
+//  WinningTicketViewController.h
+//  Jackpot
+//
+//  Created by Jorge Catalan on 4/21/16.
+//  Copyright © 2016 Jorge Catalan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WinningTicketViewController : UIViewController
+
+@end
